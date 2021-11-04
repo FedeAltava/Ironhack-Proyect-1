fdb943c28f7a8a12e671beee44a9583ee3f5b7a1
Federico Altava Redondo <fedealtava@gmail.com>
2021-11-04 12:09:26 +0100

    Just images start volwe increase score

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ window.onload = () => {
     const CANVAS_HEIGHT = canvas.height
 
     let score = 0
-
+    let  animationFrameId
     //Audio
     const backGroundSound = new Audio('sounds/backgroundSound.mp3')
     backGroundSound.volume = 0.05
@@ -18,7 +18,7 @@ window.onload = () => {
     soundSamePlace.volume = 0.05
     //loop music background --------- Preguntar
     // const backGroundRepeat = ()=>{
-        //if (typeof backGroundSound == 'boolean'){
+    //if (typeof backGroundSound == 'boolean'){
     //     backGroundSound = true;
     // }
     // else
@@ -33,36 +33,46 @@ window.onload = () => {
     //Images
     let imageBackground = new Image()
     imageBackground.src = 'images/scenary/scenary.png'
-    
+
     let characterImage = new Image()
     characterImage.src = 'images/character/player.png'
 
+    // let characterImageLeft = new Image()
+    // characterImageLeft = 'images/character/character_left.png'
+    // // const paintLeftImage = () => {
+    // //     ctx.drawImage(characterImageLeft, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    // // }
 
+    let YouWon = new Image()
+    YouWon.src = 'images/scenary/Has_ganado.jpg'
 
+    const paintYouWon = () => {
+        ctx.drawImage(YouWon, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    }
 
 
 
     //Todas las imagenes que tengo
 
     const imageLinks = [
-        { link: "images/a_images/abeja.png", name: 'abeja', letter:'A' },
-        { link: "images/a_images/aguila.png", name: 'aguila', letter:'A' },
-        { link: "images/e_images/erizo.png", name: 'erizo', letter:'A' },
-        { link: "images/e_images/elefante.png", name: 'elefante', letter:'A' },
-        { link: "images/i_images/iglu.png", name: 'iglu' , letter:'I'},
-        { link: "images/i_images/indio.png", name: 'indio', letter:'I' },
-        { link: "images/o_images/obeja.png", name: 'obeja', letter:'O' },
-        { link: "images/o_images/oso.png", name: 'oso', letter:'O' },
-        { link: "images/u_images/unicornio.png", name: 'unicornio', letter:'U' },
-        { link: "images/u_images/uva.png", name: 'uva', letter:'U' },
-        { link: "images/random/calabaza.png", name: 'calabaza', letter:'C' },
-        { link: "images/random/casa.png", name: 'casa', letter:'C' },
-        { link: "images/random/cocodrilo.png", name: 'cocodrilo', letter:'C' },
-        { link: "images/random/hormiga.png", name: 'hormiga', letter:'H'},
+        { link: "images/a_images/abeja.png", name: 'abeja', letter: 'A' },
+        { link: "images/a_images/aguila.png", name: 'aguila', letter: 'A' },
+        { link: "images/e_images/erizo.png", name: 'erizo', letter: 'A' },
+        { link: "images/e_images/elefante.png", name: 'elefante', letter: 'A' },
+        { link: "images/i_images/iglu.png", name: 'iglu', letter: 'I' },
+        { link: "images/i_images/indio.png", name: 'indio', letter: 'I' },
+        { link: "images/o_images/obeja.png", name: 'obeja', letter: 'O' },
+        { link: "images/o_images/oso.png", name: 'oso', letter: 'O' },
+        { link: "images/u_images/unicornio.png", name: 'unicornio', letter: 'U' },
+        { link: "images/u_images/uva.png", name: 'uva', letter: 'U' },
+        { link: "images/random/calabaza.png", name: 'calabaza', letter: 'C' },
+        { link: "images/random/casa.png", name: 'casa', letter: 'C' },
+        { link: "images/random/cocodrilo.png", name: 'cocodrilo', letter: 'C' },
+        { link: "images/random/hormiga.png", name: 'hormiga', letter: 'H' },
     ]
 
 
-    let arrayOfGameFigures = [];
+    let arrayOfGameFigures = []
 
     //Funciones ************************
 
@@ -76,14 +86,17 @@ window.onload = () => {
             //luego acceder a ese array con ese numero que es la posicion dentro de ese array
             //al ser un objeto que tiene la key link, podemos pasar ese link
             const imageLink = imageLinks[aleatorio].link
-            // const imageLetter = imageLetter[aleatorio].letter
+            
+            const letter = imageLinks[aleatorio].letter
+
+            console.log(letter)
+            
             //Creas una imagen con ese link que será el src de la nueva imagen
             const image = new Image()
             image.src = imageLink
-            // const letter = new Image()
-            // image.letter = imageLetter
+            
             //Creas un nuevo objeto GameFigure y le pasamos la imagen que acabamos de crear
-            const newGameFigure = new GameFigure(image)
+            const newGameFigure = new GameFigure(image,letter)
 
             //Metemos el nuevo objeto en nuestro array de gameFigures
             arrayOfGameFigures.push(newGameFigure)
@@ -91,29 +104,41 @@ window.onload = () => {
         }, 3000)
     }
 
-
-    // comprobar si estan en el mismo lugar
+    let vowelsArray=['A','E','I','O','U']
+  
+        
+   
+   
     const checkCollision = (myCharacter, gameFigure) => {
 
 
         if (myCharacter.x < gameFigure.x + gameFigure.width &&  //check lado izquierdo
             myCharacter.x + myCharacter.width > gameFigure.x &&  //check lado derecho
             myCharacter.y < gameFigure.y + gameFigure.height &&  //check lado de arriba
-            myCharacter.height + myCharacter.y > gameFigure.y) { 
-                 
-                 score++
-                if(score == 5) {
-                    alert("YOU WIN, CONGRATULATIONS!")
-                    document.location.reload()
+            myCharacter.height + myCharacter.y > gameFigure.y) {
+                if(vowelsArray.includes(gameFigure.letter)){
+                 score++   
                 }
-            gameFigure.picked =true;
+            
+            gameFigure.picked = true;
             soundSamePlace.play()
             document.getElementById('score').innerText = score
+
+
+            if (score === 5) {
+             
+                setTimeout(() => {
+                    cancelAnimationFrame(animationFrameId)
+                })
+
+                paintYouWon()
+
+            }
         }
 
     }
-    const deleteGameFigures =()=>{
-        arrayOfGameFigures = arrayOfGameFigures.filter((figure)=>{
+    const deleteGameFigures = () => {
+        arrayOfGameFigures = arrayOfGameFigures.filter((figure) => {
             return !figure.picked
         })
     }
@@ -149,6 +174,7 @@ window.onload = () => {
         moveLeft() {
 
             this.speedX = -5
+            
         }
 
         moveRight() {
@@ -182,28 +208,29 @@ window.onload = () => {
     const character = new Character(characterImage);
 
 
+
     class GameFigure {
-        constructor(image) {
+        constructor(image,letter) {
             this.width = 90;
             this.height = 90;
             this.x = Math.floor(Math.random() * (CANVAS_WIDTH - this.width))
             this.y = 10;
             this.speedY = 2;
             this.image = image;
-            // this.letter = letter;
-            this.picked =false
+            this.letter = letter;
+            this.picked = false
         }
         draw() {
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
         }
         update() {
             this.y += this.speedY
-            if(this.y + this.height > CANVAS_HEIGHT){
+            if (this.y + this.height > CANVAS_HEIGHT) {
                 this.picked = true
             }
         }
     }
-    
+
 
 
     //Este es mi loop de animación <----------------
@@ -214,7 +241,6 @@ window.onload = () => {
         character.update()
         character.checkLimits()
         character.draw()
-
         //Pintamos gameFigures
         arrayOfGameFigures.forEach((figure) => {
             figure.update()
@@ -225,8 +251,11 @@ window.onload = () => {
             checkCollision(character, figure)
         })
         deleteGameFigures()
-        requestAnimationFrame(updateCanvas)
+
+        animationFrameId = requestAnimationFrame(updateCanvas)
+         
     }
+
 
 
     //EVENT LISTENERS
@@ -238,6 +267,7 @@ window.onload = () => {
             character.moveRight();
         } else if (event.key === "ArrowLeft") {
             character.moveLeft();
+            
         }
     })
 
@@ -253,7 +283,7 @@ window.onload = () => {
 
 
 
-//Cuando choque, que sume puntuacion - suene sonido
+
 //Cuando apretes flecha izquierda que el personaje se gire a la izquierda
 
 
